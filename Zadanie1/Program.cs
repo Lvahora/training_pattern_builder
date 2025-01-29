04a7dfd9084d52e98b69789783669fa0168820b1
Lvahora <m01050242@gmail.com>
2025-01-29 17:37:50 +0300

    Добавьте файлы проекта.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+// Задание 1
+public class Car
+{
+    public int Seats { get; set; }
+    public string Engine { get; set; }
+    public bool TripComputer { get; set; }
+    public bool GPS { get; set; }
+
+    public override string ToString()
+    {
+        return $"Машина с {Seats} кресел, {Engine} двигатель, Бортовой компьютер: {TripComputer}, GPS: {GPS}";
+    }
+}
+
+public class Manual
+{
+    public int Seats { get; set; }
+    public string Engine { get; set; }
+    public string TripComputerInstructions { get; set; }
+    public string GPSInstructions { get; set; }
+    public override string ToString()
+    {
+        return $"Руководство по эксплуатации с {Seats} сиденьями, {Engine} двигателем, инструкциями бортового компьютера: {TripComputerInstructions}, инструкциями GPS: {GPSInstructions}";
+    }
+}
+
+
+public interface IBuilder
+{
+    void Reset();
+    void SetSeats(int number);
+    void SetEngine(string engine);
+    void SetTripComputer();
+    void SetGPS();
+}
+
+
+
+public class CarBuilder : IBuilder
+{
+    private Car _car;
+
+    public CarBuilder()
+    {
+        _car = new Car();
+    }
+
+    public void Reset()
+    {
+        _car = new Car();
+    }
+
+    public void SetSeats(int number)
+    {
+        _car.Seats = number;
+    }
+
+    public void SetEngine(string engine)
+    {
+        _car.Engine = engine;
+    }
+
+    public void SetTripComputer()
+    {
+        _car.TripComputer = true;
+    }
+
+    public void SetGPS()
+    {
+        _car.GPS = true;
+    }
+
+    public Car GetResult()
+    {
+        return _car;
+    }
+}
+
+public class CarManualBuilder : IBuilder
+{
+    private Manual _manual;
+
+    public CarManualBuilder()
+    {
+        _manual = new Manual();
+    }
+
+    public void Reset()
+    {
+        _manual = new Manual();
+    }
+
+    public void SetSeats(int number)
+    {
+        _manual.Seats = number;
+    }
+
+    public void SetEngine(string engine)
+    {
+        _manual.Engine = engine;
+    }
+
+    public void SetTripComputer()
+    {
+        _manual.TripComputerInstructions = "Добавить инструкцию в бортовой компьютер";
+    }
+
+    public void SetGPS()
+    {
+        _manual.GPSInstructions = "Добавьте инструкцию к GPS";
+    }
+
+
+    public Manual GetResult()
+    {
+        return _manual;
+    }
+}
+
+
+
+public class Director
+{
+    public void MakeSUV(IBuilder builder)
+    {
+        builder.Reset();
+        builder.SetSeats(4);
+        builder.SetEngine("Мощный двигатель");
+        builder.SetTripComputer();
+        builder.SetGPS();
+    }
+
+    public void MakeSportsCar(IBuilder builder)
+    {
+        builder.Reset();
+        builder.SetSeats(2);
+        builder.SetEngine("Спортивный двигатель");
+        builder.SetTripComputer();
+        builder.SetGPS();
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Director director = new Director();
+
+
+        CarBuilder carBuilder = new CarBuilder();
+        director.MakeSportsCar(carBuilder);
+        Car car = carBuilder.GetResult();
+        Console.WriteLine(car);
+
+        CarManualBuilder manualBuilder = new CarManualBuilder();
+        director.MakeSUV(manualBuilder);
+        Manual manual = manualBuilder.GetResult();
+        Console.WriteLine(manual);
+    }
+}
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HouseCatalogApp
+{
+    public class HouseType
+    {
+        public string Material { get; set; }
+        public List<string> AdditionalBuildings { get; set; } = new List<string>();
+
+        public override string ToString()
+        {
+            string buildings = AdditionalBuildings.Count > 0 ? string.Join(", ", AdditionalBuildings) : "Нет";
+            return $"{Material} дом, дополнительные постройки: {buildings}";
+        }
+    }
+
+    public static class AdditionalBuilding
+    {
+        public const string Pool = "Бассейн";
+        public const string Garage = "Гараж";
+        public const string FrontGarden = "Полисадник";
+        public const string Path = "Тропинка";
+    }
+
+
+    public class HouseCatalog
+    {
+        private List<HouseType> _houseTypes = new List<HouseType>();
+
+        public void AddHouseType(HouseType houseType)
+        {
+            _houseTypes.Add(houseType);
+        }
+
+        public void RemoveHouseType(HouseType houseType)
+        {
+            _houseTypes.Remove(houseType);
+        }
+
+        public List<HouseType> GetAllHouseTypes()
+        {
+            return _houseTypes;
+        }
+
+
+        public void PrintCatalog()
+        {
+            if (_houseTypes.Count == 0)
+            {
+                Console.WriteLine("Каталог пуст.");
+                return;
+            }
+
+            Console.WriteLine("Строительный каталог домов:");
+            foreach (var houseType in _houseTypes)
+            {
+                Console.WriteLine("- " + houseType);
+            }
+        }
+    }
+
+
+    public class CatalogFiller
+    {
+        public static void FillCatalog(HouseCatalog catalog)
+        {
+            var stoneHouse = new HouseType { Material = "Камень" };
+            stoneHouse.AdditionalBuildings.Add(AdditionalBuilding.Pool);
+            stoneHouse.AdditionalBuildings.Add(AdditionalBuilding.Garage);
+            catalog.AddHouseType(stoneHouse);
+
+            var brickHouse = new HouseType { Material = "Кирпич" };
+            brickHouse.AdditionalBuildings.Add(AdditionalBuilding.FrontGarden);
+            brickHouse.AdditionalBuildings.Add(AdditionalBuilding.Path);
+            catalog.AddHouseType(brickHouse);
+
+            var mixedHouse = new HouseType { Material = "Кирпич и камень" };
+            mixedHouse.AdditionalBuildings.Add(AdditionalBuilding.Pool);
+            mixedHouse.AdditionalBuildings.Add(AdditionalBuilding.Garage);
+            mixedHouse.AdditionalBuildings.Add(AdditionalBuilding.FrontGarden);
+            mixedHouse.AdditionalBuildings.Add(AdditionalBuilding.Path);
+            catalog.AddHouseType(mixedHouse);
+        }
+    }
+}
